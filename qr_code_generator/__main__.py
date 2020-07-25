b4742e3b405aa3b3a93b13b336a7d9a227824f3c
j.veldhuis <jobbfveldhuis@gmail.com>
2020-07-25 11:46:17 +0200

    Functioning main entry point for module

--- qr_code_generator/__main__.py
+++ qr_code_generator/__main__.py
@@ -1,23 +1,55 @@
 #!/usr/bin/env python3
 from wrapper import QrGenerator
-import sys
 import argparse
 
 
 def main():
-    """WIP: Main entry point"""
+    """Main entry function, parses arguments, creates an instance of the wrapper and requests QR codes."""
     parser = create_parser()
     args = parser.parse_args()
+
+    # When an API token is explicitly specified, set it. Else, initialize without token.
     if args.token:
         api = QrGenerator(args.token)
     else:
         api = QrGenerator()
 
+    # If a configuration file to load from has been found, load it to the app config
     if args.config:
-        print("works")
+        api.config.load(args.config)
+
+    # If output filename is specified, set filename in app
+    if args.output:
+        api.output_filename = args.output
+
+    # If verbose flag is added, overwrite configuration to set VERBOSE to True
+    if args.verbose:
+        api.config['VERBOSE'] = True
+
+    # If bulk requests are made, we should enumerate them and give them specific names
+    if args.bulk:
+        if api.output_filename:
+            name = api.output_filename
+            for i in range(1, args.bulk + 1):
+                api.output_filename = f'{name}-{i}'
+                api.request()
+        else:
+            for i in range(1, args.bulk + 1):
+                api.request()
+    else:
+        api.request()
 
 
 def create_parser():
+    """
+    Create argument parser for command line interface
+
+    Returns
+    -------
+    parser : ArgumentParser
+        The parser with arguments, that will parse the console command
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--token', help='access token for the API', type=str, metavar='')
     parser.add_argument('-c', '--config', help='relative path to config.ini file to load settings from', type=str,
@@ -29,4 +61,5 @@ def create_parser():
 
 
 if __name__ == "__main__":
+    """Main entry point, calls main entry function"""
     main()
