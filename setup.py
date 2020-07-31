f391d50931dd93ce25b7f2c16d6795eb225ec6c4
j.veldhuis <jobbfveldhuis@gmail.com>
2020-07-31 13:21:58 +0200

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,26 @@ with open("README.md", "r") as doc:
     long_description = doc.read()
 
 setuptools.setup(
-    name="qr_code_generator_api",
-    version="0.1.0",
-    author="Job Veldhuis",
-    author_email="job@baukefrederik.me",
-    description="Python wrapper for qr-code-generator.com API",
+    name='qr_code_generator_api',
+    version='0.1.1',
+    author='Job Veldhuis',
+    author_email='job@baukefrederik.me',
+    description='Python wrapper for qr-code-generator.com API',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/jobveldhuis/python-qr-code-generator-api',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+        'pyYaml',
     ],
     entry_points='''
     [console_scripts]
